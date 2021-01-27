c2187108b000789e66d72f4082a57d226ffa1f93
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-01-27 23:49:19 +0000

    added DOM elements

--- script.js
+++ script.js
@@ -6,4 +6,30 @@ const randomNumber = () => {
 
 let startButton = document.querySelector('#start');
 
-let dice1 = 
+let dice1 = document.getElementById('dice_A');
+let dice2 = document.getElementById('dice_B');
+
+startButton.onclick = () => {
+	let randomNum1 = randomNumber();
+	let randomNum2 = randomNumber();
+
+	dice1.src = `images/dice${randomNum1}.png`;
+	dice2.src = `images/dice${randomNum2}.png`;
+
+	if (randomNum1 > randomNum2) {
+		document.querySelector('h1').innerHTML = 'Player 1 Wins!!!';
+		document.getElementById('player_1').style.color = 'red';
+		document.getElementById('player_1').style.fontWeight = 'bold';
+		document.getElementById('player_2').style.color = 'green';
+	} else if (randomNum1 < randomNum2) {
+		document.querySelector('h1').innerHTML = 'Player 2 Wins!!!';
+		document.getElementById('player_2').style.color = 'red';
+		document.getElementById('player_2').style.fontWeight = 'bold';
+		document.getElementById('player_1').style.color = 'green';
+	} else if (randomNum1 == randomNum2) {
+		document.querySelector('h1').innerHTML = 'Draw!!!';
+		document.getElementById('player_2').style.color = 'yellow';
+		document.getElementById('player_2').style.fontWeight = 'bold';
+		document.getElementById('player_1').style.color = 'yellow';
+	}
+};
